2386aaf1fda43c361d8052b9e6503710fe01467a
bavykin.grisha <bavykin.grisha@syk.fi>
2022-08-28 15:57:01 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 123;
+        int b = 53;
+       if (a%10 == b%10)
+            System.out.println("true");
+        else
+            System.out.println("false");
+    }
+}
